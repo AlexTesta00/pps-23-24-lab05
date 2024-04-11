f78f213eaed612a9da3f865e9237236137dd9c1a
Alex Testa <alex.testa@studio.unibo.it>
2024-04-11 22:24:43 +0200

    Refactor

--- src/main/scala/polyglot/a01b/LogicsImpl.scala
+++ src/main/scala/polyglot/a01b/LogicsImpl.scala
@@ -28,7 +28,7 @@ object Cell:
 
 
   private case class CellImpl(override val position: Point2D,
-                                  override val isMine: Boolean,
+                              override val isMine: Boolean,
                                   var isRevealed: Boolean,
                                   var isFlagged: Boolean) extends Cell:
     override def reveal(): Unit = isRevealed = true
@@ -44,50 +44,68 @@ object GameCell:
   def mine(position: Point2D): Cell = Cell(position, isMine = true, isRevealed = false, isFlagged = false)
   def default(position: Point2D): Cell = Cell(position, isMine = false, isRevealed = false, isFlagged = false)
 
+trait Board:
 
+  def board: Sequence[Cell]
+  def minesPosition(): Sequence[Point2D]
+  def allNeighboursOf(x: Int, y: Int): Sequence[Cell]
+  def cellInfo(x: Int, y: Int): CellInGame
+
+object Board:
+  def apply(size: Int, mines: Int): Board = GameBoard(size, mines)
+
+  private case class GameBoard(size: Int, mines: Int) extends Board:
+
+    private val minesPositions: Sequence[Point2D] = randomPosition(mines)
+
+    private def randomPosition(numberOfMines: Int): Sequence[Point2D] =
+      val random = scala.util.Random
+      var positions = Sequence.empty[Point2D]
+      while positions.size() <= numberOfMines - 1 do
+        val point = Point2D(random.nextInt(size), random.nextInt(size))
+        if !positions.contains(point) then positions = positions.concat(Sequence.apply(point))
+      positions
+
+    private val gameBoard: Sequence[Cell] =
+      var cells = Sequence.empty[Cell]
+      0 until size map (x =>
+        0 until size map (y =>
+          if minesPositions.contains(Point2D(x, y)) then
+            cells = cells.concat(Sequence.apply(GameCell.mine(Point2D(x, y))))
+          else
+            cells = cells.concat(Sequence.apply(GameCell.default(Point2D(x, y))))
+          )
+        )
+      cells
+
+    override def minesPosition(): Sequence[Point2D] = minesPositions
+
+    override def allNeighboursOf(x: Int, y: Int): Sequence[Cell] =
+      board.filter(_.isNeighbourOf(GameCell.default(Point2D(x, y))))
+
+    override def cellInfo(x: Int, y: Int): CellInGame =
+      val cell = board.find(_.position == Point2D(x, y)).get()
+      val neighbours = allNeighboursOf(cell.position.x, cell.position.y).filter(_.isMine).size()
+      CellInGame(cell.isRevealed, cell.isFlagged, cell.isMine, neighbours)
+
+    override def board: Sequence[Cell] = gameBoard
 
 
 class LogicsImpl(private val size: Int, private val mines: Int) extends Logics:
 
-  private val minesPositions: Sequence[Point2D] = randomPosition(mines)
-  private val board: Sequence[Cell] =
-    var cells = Sequence.empty[Cell]
-    0 until size map(x =>
-      0 until size map(y =>
-        if minesPositions.contains(Point2D(x, y)) then
-          cells = cells.concat(Sequence.apply(GameCell.mine(Point2D(x, y))))
-        else
-          cells = cells.concat(Sequence.apply(GameCell.default(Point2D(x, y))))
-        )
-      )
-    cells
-
-  private def randomPosition(numberOfMines: Int): Sequence[Point2D] =
-    val random = scala.util.Random
-    var positions = Sequence.empty[Point2D]
-    while positions.size() <= numberOfMines - 1 do
-      val point = Point2D(random.nextInt(size), random.nextInt(size))
-      if !positions.contains(point) then positions = positions.concat(Sequence.apply(point))
-    positions
-
-  private def allNeighboursOf(point: Point2D): Sequence[Cell] = board.filter(_.isNeighbourOf(GameCell.default(point)))
+  private val minesBoard: Board = Board(size, mines)
 
   override def hit(x: Int, y: Int): Unit =
     val currentPosition = Point2D(x, y)
-    val neighbours = allNeighboursOf(currentPosition)
-    val currentCell = board.find(_.position == currentPosition).get()
+    val neighbours = minesBoard.allNeighboursOf(x, y)
+    val currentCell = minesBoard.board.find(_.position == currentPosition).get()
     if !currentCell.isFlagged then
       currentCell.reveal()
-      board.filter(c => neighbours.contains(c) && !c.isMine).map(_.reveal())
+      minesBoard.board.filter(c => neighbours.contains(c) && !c.isMine).map(_.reveal())
 
-  override def flagCell(x: Int, y: Int): Unit = board.map(c => if c.position == Point2D(x, y) then c.flag())
-  override def won(): Boolean = board.filter(c => !c.isMine && !c.isRevealed).size() == 0
+  override def flagCell(x: Int, y: Int): Unit = minesBoard.board.map(c => if c.position == Point2D(x, y) then c.flag())
+  override def won(): Boolean = minesBoard.board.filter(c => !c.isMine && !c.isRevealed).size() == 0
 
-  override def lost(): Boolean = board.filter(c => c.isMine && c.isRevealed).size() != 0
+  override def lost(): Boolean = minesBoard.board.filter(c => c.isMine && c.isRevealed).size() != 0
 
-  override def getCell(x: Int, y: Int): CellInGame =
-    val cell = board.find(_.position == Point2D(x,y)).get()
-    val neighbours = allNeighboursOf(cell.position).filter(_.isMine).size()
-    CellInGame(cell.isRevealed, cell.isFlagged, cell.isMine, neighbours)
-
-
+  override def getCell(x: Int, y: Int): CellInGame = minesBoard.cellInfo(x, y)
